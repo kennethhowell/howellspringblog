96571cd20ee2922c436a933803bd8b6d5023b48d
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-01-27 16:12:43 -0600

    worked dice bonus

--- src/main/java/com/codeup/howellspringblog/controllers/DiceController.java
+++ src/main/java/com/codeup/howellspringblog/controllers/DiceController.java
@@ -22,16 +22,33 @@ public class DiceController {
     @GetMapping("/roll-dice/{number}")
     public String diceCheck(@PathVariable int number, Model model){
         int userRoll = number;
+        int userCounter = 0;
         int computerRoll = (int) (Math.random() * 6) + 1;
+        int computerRoll2 = (int) (Math.random() * 6) + 1;
+        int computerRoll3 = (int) (Math.random() * 6) + 1;
+
         String diceresult = "";
 
 
-        boolean dicegood = computerRoll == userRoll;
+        boolean dicegood = false;
+
+        if (userRoll == computerRoll){
+            dicegood = true;
+            userCounter++;
+        }
+        if (userRoll == computerRoll2){
+            dicegood = true;
+            userCounter++;
+        }
+        if (userRoll == computerRoll3){
+            dicegood = true;
+            userCounter++;
+        }
 
         if (dicegood){
-            diceresult = "Good job! You rolled " + userRoll + " and the computer's roll was " + computerRoll;
+            diceresult = "Good job! You guessed " + userRoll + " and the computer's rolls were " + computerRoll + ", " + computerRoll2 + ", and " + computerRoll3 + ". You matched " + userCounter + " rolls!";
         } else {
-            diceresult = "Bummer dude, you rolled " + userRoll + " and the computer's roll was " + computerRoll;
+            diceresult = "Bummer dude, you guessed " + userRoll + " and the computer's rolls were " + computerRoll + ", " + computerRoll2 + ", and " + computerRoll3 + ".";
         }
 
         model.addAttribute("diceresult", diceresult);
