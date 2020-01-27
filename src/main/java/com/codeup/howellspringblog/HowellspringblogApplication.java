d3f0d89699171bb10852ce1870f642cb02863249
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-01-27 09:53:58 -0600

    example.properties added

---
+++ src/main/java/com/codeup/howellspringblog/HowellspringblogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.howellspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowellspringblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowellspringblogApplication.class, args);
+    }
+
+}
